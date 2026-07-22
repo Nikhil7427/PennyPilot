b7b1ba4c810227fbc1f7b25486ffeda7e775d5e2
Nikhil7427 <nikhilmaurya1010@gmail.com>
2026-07-22 20:29:07 +0530

    Implement user registration with JWT authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,60 @@
-export const registerUser = async(req, res) => {
+import { db } from "../db/index.js";
+import { users } from "../db/schema.js";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+
+export const registerUser = async (req, res) => {
     try {
+        const { name, email, password } = req.body;
+
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
+
+        const existingUser = await db.query.users.findFirst({
+            where: eq(users.email, email),
+        })
+
+        if (existingUser) {
+            return res.status(409).json({
+                success: false,
+                message: "User Already exist",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await db.insert(users).values({
+            name, 
+            email,
+            password: hashedPassword,
+        }).returning();
+
+        const token = jwt.sign(
+            {
+                id: newUser[0].id,
+                email: newUser[0].email,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        );
+
         res.status(201).json({
             success: true,
-            message: "Register Api is working",
+            message: "User registered successfully",
+            token,
+            user: {
+                id: newUser[0].id,
+                name: newUser[0].name,
+                email: newUser[0].email,
+            },
         });
     } catch (error) {
         console.error(error);
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,11 +1,14 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import * as schema from "./schema.js";
 
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, {
+  schema,
+});
