05355290353e4dd254efcf115ff724fdb9519792
Nikhil7427 <nikhilmaurya1010@gmail.com>
2026-07-26 14:10:01 +0530

    Implement expense CRUD APIs with JWT authorization

--- backend/src/controllers/expense.controller.js
+++ backend/src/controllers/expense.controller.js
@@ -1,5 +1,8 @@
+import { defineConfig } from 'drizzle-kit';
 import { db } from '../db/index.js'
 import { expenses } from '../db/schema.js';
+import { eq, and } from 'drizzle-orm';
+import e from 'express';
 
 export const createExpense = async (req, res) => {
     try {
@@ -35,6 +38,117 @@ export const createExpense = async (req, res) => {
         res.status(500).json({
             success: false,
             message: "Internal server error",
+        });
+    }
+};
+
+export const getExpenses = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const userExpense = await db
+            .select()
+            .from(expenses)
+            .where(eq(expenses.userId, userId));
+
+        res.status(200).json({
+            success: true,
+            count: userExpense.length,
+            expenses: userExpense,
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+};
+
+export const updateExpense = async (req, res) => {
+    try {
+        const expenseId = Number(req.params.id);
+        const userId = req.user.id;
+
+        const { title, amount, category, date, notes } = req.body;
+        
+        const existingExpense = await db.query.expenses.findFirst({
+            where: and(
+                eq(expenses.id, expenseId),
+                eq(expenses.userId, userId)
+            ),
+        });
+
+        if(!existingExpense){
+            return res.status(404).json({
+                success: false,
+                message: "Expense not found",
+            });
+        }
+
+        const updatedExpense = await db
+            .update(expenses)
+            .set({
+                title,
+                amount,
+                category,
+                date,
+                notes,
+            })
+            .where(eq( expenses.id ,expenseId ))
+            .returning();
+
+        res.status(200).json({
+            success: true,
+            message: "Expense updated successfully",
+            expense: updatedExpense[0],
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+};
+
+export const deleteExpense = async(req, res) => {
+    try {
+        const expenseId = Number(req.params.id);
+        const userId = req.user.id;
+
+        const existingExpense = await db.query.expenses.findFirst({
+            where: and(
+                eq(expenses.id, expenseId),
+                eq(expenses.userId, userId)
+            ),
+        });
+
+        if(!existingExpense){
+            return res.status(404).json({
+                success: false,
+                message: "Expense not found",
+            });
+        }
+
+        await db   
+            .delete(expenses)
+            .where(eq(expenses.id, expenseId));
+
+        res.status(200).json({
+            success: true,
+            message: "Expense deleted successfully",
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
         });
     }
 }
